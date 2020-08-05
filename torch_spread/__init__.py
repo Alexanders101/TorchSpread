1dfbbdada101631f06e0c9beadfe397d732958c8
Alex Shmakov <Alexanders101@gmail.com>
2020-08-05 14:04:53 -0700

    Added multiprocessing import to init file

--- torch_spread/__init__.py
+++ torch_spread/__init__.py
@@ -2,3 +2,4 @@ from .NetworkManager import NetworkManager
 from .ManagerTools import PlacementStrategy, TrainingWrapper, DataParallelWrapper, SpreadModule
 from .NetworkClient import NetworkClient, RemoteClient
 from .BufferTools import Buffer
+from .utilities import mp_ctx, multiprocessing
