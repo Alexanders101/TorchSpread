2844c1f2157ca9329a2cfe7f33fec3e98ae3edaf
Alex Shmakov <Alexanders101@gmail.com>
2020-08-05 18:48:58 -0700

    Added forkserver preloads

--- torch_spread/utilities.py
+++ torch_spread/utilities.py
@@ -12,6 +12,7 @@ from multiprocessing.reduction import ForkingPickler
 
 # Setup fork-server mode for the entire library
 multiprocessing = mp_ctx = torch.multiprocessing.get_context('forkserver')
+mp_ctx.set_forkserver_preload(["torch", "numpy", "msgpack", "pickle"])
 
 # Recursive definitions not supported yet, so I use Tensor for the subtypes
 # But the correct type should have BufferType where Tensor is.
